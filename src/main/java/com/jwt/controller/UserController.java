de87fd00f1a82aa17d43e43edaf269057ad4bb65
Ihwardas <ishwar39@gmail.com>
2024-10-27 14:57:10 +0530

    Add signUp and getString method

---
+++ src/main/java/com/jwt/controller/UserController.java
@@ -0,0 +1,46 @@
+package com.jwt.controller;
+
+import com.jwt.entity.AppUser;
+import com.jwt.payload.LoginDto;
+import com.jwt.repository.AppUserRepository;
+import com.jwt.service.AppUserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/user")
+public class UserController {
+    private AppUserRepository appUserRepository;
+    private AppUserService appuserService;
+
+
+    public UserController(AppUserRepository appUserRepository, AppUserService appuserService) {
+        this.appUserRepository = appUserRepository;
+        this.appuserService = appuserService;
+    }
+    @PostMapping("/signup")
+    public ResponseEntity<?>createUser(@RequestBody AppUser user) {
+        Optional<AppUser> opUsername = appUserRepository.findByUsername(user.getUsername());
+        if (opUsername.isPresent()) {
+            return new ResponseEntity<>("this username is allready is present", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        Optional<AppUser> opEmail = appUserRepository.findByEmail(user.getEmail());
+        if (opEmail.isPresent()) {
+            return new ResponseEntity<>("this email is allready present", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        AppUser proSaved = appuserService.createProfile(user);
+        return new ResponseEntity<>(proSaved,HttpStatus.CREATED);
+
+    }
+    @GetMapping("/message")
+    public String getMessage(){
+        return "hello";
+    }
+    public ResponseEntity<String> login(@RequestBody LoginDto dto){
+        appuserService.
+    }
+}
--- src/main/java/com/jwt/service/AppUserService.java
+++ src/main/java/com/jwt/service/AppUserService.java
@@ -1,5 +1,6 @@
 package com.jwt.service;
 
+import com.jwt.entity.AppUser;
 import com.jwt.repository.AppUserRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
@@ -13,4 +14,10 @@ public class AppUserService {
         this.modelMapper = modelMapper;
     }
 
+
+    public AppUser createProfile(AppUser user) {
+        AppUser save = appUserRepository.save(user);
+        return save;
+
+    }
 }
